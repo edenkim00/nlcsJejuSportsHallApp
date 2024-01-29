39d16c18cd9b9eafd85d2a0daa05813445f44d0c
edenkim00 <eotjd0986@kaist.ac.kr>
2024-01-29 20:09:45 +0900

    feat: api manager

--- src/api/index.js
+++ src/api/index.js
@@ -3,21 +3,36 @@ const BASE_URL =
   'https://lspy262udm2a3l3xf6fcxdgzry0yxebi.lambda-url.ap-northeast-2.on.aws';
 
 export default class APIManager {
+  /* voting */
   static async vote(voteBody) {
     return await APIRequestHelper.request('/app/vote', 'POST', true, voteBody);
   }
 
-  static async getUserInfo() {
-    return await APIRequestHelper.request('/app/user-info', 'GET', true);
+  static async getConfirmedResult(category_id) {
+    return await APIRequestHelper.request(
+      '/app/confirmed-result',
+      'GET',
+      true,
+      undefined,
+      {
+        category_id,
+      },
+    );
   }
 
-  static async login(email, password) {
-    return await APIRequestHelper.request('/app/signin', 'POST', false, {
-      email,
-      password,
-    });
+  static async getVoteCategories(grade) {
+    return await APIRequestHelper.request(
+      '/app/vote-categories',
+      'GET',
+      true,
+      undefined,
+      {
+        grade,
+      },
+    );
   }
 
+  /* user */
   static async signUp(email, password, name, sex, graduationYear) {
     return await APIRequestHelper.request('/app/user-signup', 'POST', false, {
       email,
@@ -28,8 +43,19 @@ export default class APIManager {
     });
   }
 
+  static async login(email, password) {
+    return await APIRequestHelper.request('/app/signin', 'POST', false, {
+      email,
+      password,
+    });
+  }
+
   static async deleteAccount() {
     return await APIRequestHelper.request('/app/delete-account', 'POST', true);
+  }
+
+  static async getUserInfo() {
+    return await APIRequestHelper.request('/app/user-info', 'GET', true);
   }
 
   static async changePassword(email, newPassword) {
@@ -56,38 +82,38 @@ export default class APIManager {
     );
   }
 
-  static async voteChange(voteBody) {
+  /* admin */
+  static async confirm(confirmedData) {
     return await APIRequestHelper.request(
-      '/app/vote-change',
-      'PATCH',
+      '/app/confirm',
+      'POST',
       true,
-      voteBody,
+      confirmedData,
     );
   }
 
-  static async reportVoteResult(email, year, month) {
+  static async postVoteCategories(
+    name,
+    grade,
+    opened_dt = undefined,
+    deadline,
+  ) {
+    return await APIRequestHelper.request('/app/open-vote', 'POST', true, {
+      name,
+      grade,
+      opened_dt,
+      deadline,
+    });
+  }
+
+  static async reportVoteResult(email, category_id) {
     return await APIRequestHelper.request(
       '/app/report-vote-result',
       'POST',
       true,
       {
         email,
-        year,
-        month,
-      },
-    );
-  }
-
-  static async getVotingResult(grade, year, month) {
-    return await APIRequestHelper.request(
-      '/app/vote-result',
-      'GET',
-      true,
-      undefined,
-      {
-        grade,
-        year,
-        month,
+        category_id,
       },
     );
   }
